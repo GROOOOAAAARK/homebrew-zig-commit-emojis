19f6458ee35dffc7eb3f8a5fe99c6bcd60d9a018
GRK <gitkey.aluminum361@passmail.net>
2026-09-18 23:34:42 +0200

    fix: remove default comments and address offenses

--- Formula/zig-commit-emojis.rb
+++ Formula/zig-commit-emojis.rb
@@ -1,8 +1,5 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://docs.brew.sh/rubydoc/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ZigCommitEmojis < Formula
-  desc "⚡ Get the best suited emoji for git commits (inspired by gitmoji), made with Ziglang"
+  desc "Get the best suited emoji for git commits"
   homepage "https://github.com/GROOOOAAAARK/zig-commit-emojis"
   url "https://github.com/grooooaaaark/zig-commit-emojis/archive/refs/tags/0.4.0.tar.gz"
   sha256 "d3b1c0a6d888a585cd05508aef04ae050d171eed14d02f35661a9cdaba9ca2b7"
@@ -30,14 +27,6 @@ class ZigCommitEmojis < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test zig-commit-emojis`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
     system "false"
   end
 end
