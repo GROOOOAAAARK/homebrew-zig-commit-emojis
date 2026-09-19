3727651850f2c52c824bd9d8c9c7e23f9015889e
GRK <gitkey.aluminum361@passmail.net>
2026-09-19 15:23:42 +0200

    fix: formula

--- Formula/zig-commit-emojis.rb
+++ Formula/zig-commit-emojis.rb
@@ -5,28 +5,19 @@ class ZigCommitEmojis < Formula
   sha256 "d3b1c0a6d888a585cd05508aef04ae050d171eed14d02f35661a9cdaba9ca2b7"
   license "MIT"
 
-  depends_on "ruby"
+  depends_on "zig@0.16" => :build
 
   deny_network_access!
 
   def fetch
-    ENV["BUNDLE_PATH"] = ".bundle"
-
-    system "bundle", "cache", "--no-install"
+    system "zig", "build", "--fetch"
   end
 
   def install
-    ENV["GEM_HOME"] = libexec
-
-    system "bundle", "install", "--local"
-    system "gem", "build", "#{name}.gemspec"
-    system "gem", "install", "--ignore-dependencies", "#{name}-#{version}.gem"
-
-    bin.install libexec/"bin/#{name}"
-    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+    system "zig", "build", *std_zig_args
   end
 
   test do
-    system "false"
+    assert_match "USAGE:", shell_output("#{bin}/zig-commit-emoji --help")
   end
 end
